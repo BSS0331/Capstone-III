5c0d27481f187c51ab955d218d9383b756f1fe7a
Seop <bsseop0331@naver.com>
2024-04-29 09:14:12 +0900

    04.28 업로드분

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -24,9 +24,12 @@ const SignUpScreen = () => {
     if (!password) {
       newErrors.password = "사용할 비밀번호를 입력해 주세요.";
       isValid = false;
-    } else if (/[^a-zA-Z0-9]/.test(password)) {
-       newErrors.password = "비밀번호에는 특수 문자를 포함할 수 없습니다.";
+    } else if (password.length < 8) {
+       newErrors.password = "비밀번호는 최소 8자 이상이어야 합니다.";
        isValid = false;
+    } else if (/[^a-zA-Z0-9]/.test(password)) {
+      newErrors.password = "비밀번호에는 특수 문자를 포함할 수 없습니다.";
+      isValid = false;
     }
     if (password !== confirmPassword) {
       newErrors.confirmPassword = "비밀번호가 일치하지 않습니다.";
@@ -106,6 +109,7 @@ const styles = StyleSheet.create({
     padding: 15,
     borderRadius: 5,
     alignItems: 'center',
+    width: '100%',
   },
   buttonText: {
     color: '#4E348B',
